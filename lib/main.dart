8400f467a7f6cc56c62ad83605df7d4876f06714
Timur Omarbayev <timur.o1999@gmail.com>
2022-11-27 22:56:58 +0600

    Added statefull widget and image with button

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,33 @@ class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue.shade900,
         title: const Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          print('I got clicked!');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
